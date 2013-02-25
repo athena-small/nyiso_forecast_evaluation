2b8bb28c8742904ca67eb4f0efd3104e479381ef
arthursmalliii <arthur.small@ventirisk.com>
2013-02-25 12:41:02 -0500

    Glitch found involving transition from EDT to EST.
    Options include:
    (i) Carefully write code that deals with the issue properly.
    (ii) Split dataset in two, analyze EDT and EST dates separately.
    (iii) Write code that IDs transition dates, throws them out; make date table with a few dates missing.
    
    I'm leaning towards (ii): the transitions between EDT and EST have real meaning for patterns of power consumption.

--- dataPrep.R
+++ dataPrep.R
@@ -1,8 +1,7 @@
 #  File dataPrep.R
 #  Fetches NYISO load forecasts and obs from local directory, creates data array
 #  Array dimensions are: 
-#    dateTime t: date+time for which forecast is valid. Class: POSIX. Range: period of record
-
+#    dateTime validDate: date for which forecast is valid. Class: POSIX. Range: period of record
 #    hour hr = 0, ..., 23: hour for which forecast is valid. Class: factor.
 #    zone zone = 1, ..., 11  : NYISO load control region. Class: factor. 
 #    forecast lag time k=0,..., 6; number of days between forecast issued-on and valid-for dates. Class: factor.
@@ -13,15 +12,14 @@
 
 # Initialize parameters
 timeZone <- 'America/New_York'
-seriesStartDate <- ISOdate(2013,2,16,0,0,0,tz=timeZone)
-seriesEndDate   <- ISOdate(2013,2,24,0,0,0,tz=timeZone)
+seriesStartDate <- ISOdate(2012,11,4,0,0,0,tz=timeZone)
+seriesEndDate   <- ISOdate(2012,11,4,0,0,0,tz=timeZone)
 nZones     <- 11   # Number of NYISO load control zones
 nHrs       <- 24   # Hrs/day, = number of forecasts issued per forecasted day
 # Actually, nHrs = 25, to allow for the one day per year when the clock transitions from EDT to EST
-nForecasts <-  6   # Number of days of forecasts in each NYISIO file
-nLags      <- nForecasts+1  # Cumulative number of forecasts issued per target hour, 
-                            #   counting the verifying observation as lag 0
-nSecsPerDay <- 3600*24
+nForecasts <-  6 # Number of days of forecasts in each NYISIO file
+nLags <- nForecasts+1  # Cumulative number of forecasts issued per target hour, counting the verifying observation as lag=0
+nSecsPerDay <- 3600*24 # Useful in conversions between date classes
 
 nFiles <- as.integer(seriesEndDate - seriesStartDate)+1
 nValidDates <- nFiles+nForecasts-1
@@ -35,8 +33,7 @@ fileHeader <- read.table(URL, header = TRUE, sep = ",",nrows=1)
 zoneNames <- names(fileHeader[1+1:nZones])
 
 URLdirLoad <- "http://mis.nyiso.com/public/csv/palIntegrated/"
-
-
+localLoadDataDir <-"./Data/"
 # Prepare a big array into which to load the forecasts and obs;
 # lag0 corresponds to the verifying observation
 dimnames = list(
@@ -44,11 +41,8 @@ dimnames = list(
      ,paste('lag',nForecasts:0,sep="")
      ,zoneNames
      ,paste('hr',0:(nHrs-1),sep=""))
-
 big.array <- array(dim=c(nValidDates,nLags,nZones,nHrs),dimnames=dimnames)
-dim(big.array)
 dimnames(big.array)[[2]][7] <- "obs"
-dimnames(big.array)
 
 # For each date for which forecast file is issued: fetch data, load into big array
 for(fileNumber in 1:nFiles){
@@ -66,76 +60,90 @@ for(fileNumber in 1:nFiles){
                big.array[dateNumber,nLags-lag,zone,1:24] <- as.matrix(fcsts[(1:24)+(lag-1)*24,zone+1])
           }
      }
-
+     
      # Now fetch the obs:
-     URL <- paste(URLdirLoad,Yr,Mo,Day,"palIntegrated.csv", sep="")  # URL of the NYISO load obs csv file
+#     URL <- paste(URLdirLoad,Yr,Mo,Day,"palIntegrated.csv", sep="")  # URL of the NYISO load obs csv file
+     URL <- paste(localLoadDataDir,Yr,Mo,"01palIntegrated_csv/",Yr,Mo,Day,"palIntegrated.csv", sep="")  # local location of downloaded & unzipped NYISO load obs csv file
      loads <- read.table(URL, header = TRUE, sep = ",")
      for(hour in 0:23){
           big.array[fileNumber,'obs',1:nZones,hour] <- as.matrix(loads[hour*nZones+1:nZones,5])
      }
-     # fileNumber
-     # big.array[fileNumber,'obs',]
 }
 
-length(big.array[is.na(big.array['Valid 2011-03-13',,'N.Y.C.',1])==TRUE])
+N <- nrow(big.array) # = length(allValidDates)
+rowsWithNAs <- c(1:5,(N-4):N)
+rowsWithNAs 
+big.array[-rowsWithNAs,,'N.Y.C.','hr16']
+dim(big.array)
+dim(big.array[-rowsWithNAs,,,])
+big.array <- big.array[-rowsWithNAs,,,]
 
-test <- c('Valid 2011-03-12','Valid 2011-03-13')
-big.array[is.na(big.array[test,-7,'N.Y.C.',1])==TRUE]
+big.array['Valid 2012-11-04',,'N.Y.C.','hr16']
+big.array[,,'N.Y.C.','hr16']
+small.array <- big.array[,,'N.Y.C.','hr16']
+rowTest
+rowTest <- c('Valid 2012-11-04','Valid 2012-11-05')
+small.array[rowTest, ]
 
-big.array[,,'N.Y.C.','hr17']
-attributes(big.array)$dim
-
+small.array[, 'lag5']['Valid 2012-11-04']
+dimnames(small.array)
 save(big.array,file='load_forecasts.dat')
 
-######### DEPRECATED CODE ############
-test.array[ ,'obs'][is.na(test.array[,'obs']==TRUE)]
-is.na(test.array[,'obs']==TRUE
-     
-# Convert partially filled array into an xts object
-# Alas, function as.xts() doesn't work on arrays, so need first to convert to a data frame 
-temp.df <- as.data.frame(temp.array)
-temp.xts <- as.xts(temp.df, order.by=dates)
+# length(big.array[is.na(big.array['Valid 2011-03-13',,'N.Y.C.',1])==TRUE])
+# test <- c('Valid 2011-03-12','Valid 2011-03-13')
+# big.array[is.na(big.array[test,-7,'N.Y.C.',1])==TRUE]
+# attributes(big.array)$dim
 
-
-head(temp.xts['2013-02-23'])
-tail(temp.xts)$'hr6'
-
-str(temp.xts)
-
-dim(temp.df[1])
-temp.df <- as.data.frame(temp.array)
-xtsible(temp.df)
-
-
-
-validTime <- as.POSIXct(fcsts$Time.Stamp, format="%m/%d/%Y %H:%M", tz='America/New_York')
-fcsts.xts <- as.xts(fcsts[2:12],order.by=validTime)
-# str(fcsts.xts)
-
-# head(fcsts.xts['/2013-02-20','North'])
-
-
-tail(dateTime)
-tail(dateTime) - issueDatetime
-
-temp.df[1:3]
-nrow(temp.df)
-nrows(temp.array)
-dimnames(temp.array)
-df <- data.frame(11,7)
-temp.xts <- as.xts(temp.df, order.by=dateTime[1:2])
-temp.xts
-nrow(temp.array)
-
-# m = c(10,20,30) # means for each of the 3 point locations
-Reps <- length(space)*length(dateTime)/length(m)
-mydata = rnorm(length(space)*length(dateTime),mean=rep(m, Reps))
-IDs = paste("ID",1:length(mydata))
-#  mydata = data.frame(values = signif(mydata,3), ID=IDs)
-# length(mydata$values)+length(mydata$ID)
-length(fcsts[,2])
-
-mydata <- as.vector(fcsts[2:12])
-length(mydata)
-
-nrow(fcsts[2:12])
+# 
+# ######### DEPRECATED CODE ############
+# test.array[ ,'obs'][is.na(test.array[,'obs']==TRUE)]
+# is.na(test.array[,'obs']==TRUE
+#      
+# # Convert partially filled array into an xts object
+# # Alas, function as.xts() doesn't work on arrays, so need first to convert to a data frame 
+# temp.df <- as.data.frame(temp.array)
+# temp.xts <- as.xts(temp.df, order.by=dates)
+# 
+# 
+# head(temp.xts['2013-02-23'])
+# tail(temp.xts)$'hr6'
+# 
+# str(temp.xts)
+# 
+# dim(temp.df[1])
+# temp.df <- as.data.frame(temp.array)
+# xtsible(temp.df)
+# 
+# 
+# 
+# validTime <- as.POSIXct(fcsts$Time.Stamp, format="%m/%d/%Y %H:%M", tz='America/New_York')
+# fcsts.xts <- as.xts(fcsts[2:12],order.by=validTime)
+# # str(fcsts.xts)
+# 
+# # head(fcsts.xts['/2013-02-20','North'])
+# 
+# 
+# tail(dateTime)
+# tail(dateTime) - issueDatetime
+# 
+# temp.df[1:3]
+# nrow(temp.df)
+# nrows(temp.array)
+# dimnames(temp.array)
+# df <- data.frame(11,7)
+# temp.xts <- as.xts(temp.df, order.by=dateTime[1:2])
+# temp.xts
+# nrow(temp.array)
+# 
+# # m = c(10,20,30) # means for each of the 3 point locations
+# Reps <- length(space)*length(dateTime)/length(m)
+# mydata = rnorm(length(space)*length(dateTime),mean=rep(m, Reps))
+# IDs = paste("ID",1:length(mydata))
+# #  mydata = data.frame(values = signif(mydata,3), ID=IDs)
+# # length(mydata$values)+length(mydata$ID)
+# length(fcsts[,2])
+# 
+# mydata <- as.vector(fcsts[2:12])
+# length(mydata)
+# 
+# nrow(fcsts[2:12])
