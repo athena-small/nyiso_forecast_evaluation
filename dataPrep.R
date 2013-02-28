e5b73031c6ca052a2da8bbff6343b5ad64ff3a73
arthursmalliii <arthur.small@ventirisk.com>
2013-02-28 16:17:12 -0500

    Now creating allObs.xts: an xts object containing all load observations.
    Next up: need to fix logical glitch that causes same file to be retrieved twice on day of transition from EDT to EST.

--- dataPrep.R
+++ dataPrep.R
@@ -9,39 +9,83 @@
 #library(plyr)
 library(xts)
 
+# Initialize parameters ---------------------------------------------------
 
-# Initialize parameters
 timeZone <- 'America/New_York'
-seriesStartDate <- ISOdate(2012,11,4,0,0,0,tz=timeZone)
-seriesEndDate   <- ISOdate(2012,11,4,0,0,0,tz=timeZone)
+Sys.setenv(TZ=timeZone)
 nZones     <- 11   # Number of NYISO load control zones
-nHrs       <- 24   # Hrs/day, = number of forecasts issued per forecasted day
-# Actually, nHrs = 25, to allow for the one day per year when the clock transitions from EDT to EST
-nForecasts <-  6 # Number of days of forecasts in each NYISIO file
-nLags <- nForecasts+1  # Cumulative number of forecasts issued per target hour, counting the verifying observation as lag=0
+nHrs       <- 24   # Hrs/day, = number of forecasts issued per (normal) forecasted day
+# N.B. nHrs = 23 or 25 when clock transitions between Daylight Savings Time and EST
+# nForecasts <-  6 # Number of days of forecasts in each NYISIO file
+# nLags <- nForecasts+1  # Cumulative number of forecasts issued per target hour, counting the verifying observation as lag=0
 nSecsPerDay <- 3600*24 # Useful in conversions between date classes
 
-nFiles <- as.integer(seriesEndDate - seriesStartDate)+1
-nValidDates <- nFiles+nForecasts-1
-allValidDates <- seriesStartDate + nSecsPerDay*0:(nValidDates-1)
-allValidDates
-
-# Fetch the names of the eleven NYISO zones (date is arbitrary)
-URLdir <- "http://mis.nyiso.com/public/csv/isolf/"  # Location of NYISO files
-URL <- paste(URLdir,'20130224isolf.csv', sep="")    # URL of a NYISO load forecast csv file
-fileHeader <- read.table(URL, header = TRUE, sep = ",",nrows=1)
-zoneNames <- names(fileHeader[1+1:nZones])
-
-URLdirLoad <- "http://mis.nyiso.com/public/csv/palIntegrated/"
-localLoadDataDir <-"./Data/"
-
+# Locations of NYISO forecast and load files
+fcstURLdir   <- "http://mis.nyiso.com/public/csv/isolf/"      
+loadURLdir   <- "http://mis.nyiso.com/public/csv/palIntegrated/"
+loadLocalDir <-"./Data/"
+# Fetch the names of the eleven NYISO zones from a forecast file (any will do, date is arbitrary)
+fcstURL <- paste(fcstURLdir,'20130224isolf.csv', sep="")    # URL of a sample load forecast csv file
+fcstFileHeader <- read.table(fcstURL, header = TRUE, sep = ",",nrows=1,as.is=c(1))
+zoneNames <- names(fcstFileHeader[1+1:nZones])
+rm(fcstFileHeader)
 
 
 # MAKE LONG DATA FRAME ----------------------------------------------------
 
-dfColnames <- c('ID','issuedDateTime','validDateTime','zone','loadMW')
-matrix()
+seriesStartDate <- ISOdate(2012,11,4,0,0,0,tz=timeZone)
+seriesEndDate   <- ISOdate(2012,11,6,0,0,0,tz=timeZone)
+nFiles <- as.integer(seriesEndDate - seriesStartDate)+1
+# nValidDates <- nFiles+nForecasts-1
+# allValidDates <- seriesStartDate + nSecsPerDay*0:(nValidDates-1)
+# allValidDates
 
+
+as.integer(seriesEndDate-seriesStartDate)
+# For each date for which forecast file is issued: fetch data, load into big array
+# fileNumber<-0
+for(fileNumber in 0:(nFiles-1)){
+     # Fetch the day's forecast file from NYISO, read values into temporary table
+     fileDate  <- seriesStartDate + fileNumber*nSecsPerDay
+     Yr  <- strftime(fileDate,format='%Y')
+     Mo  <- strftime(fileDate,format='%m')
+     Day <- strftime(fileDate,format='%d')
+
+     # Fetch the obs from local location of downloaded & unzipped NYISO load obs csv file
+     URL <- paste(loadLocalDir,Yr,Mo,"01palIntegrated_csv/",Yr,Mo,Day,"palIntegrated.csv", sep="")
+     loads <- read.table(URL,header=TRUE,sep=",",as.is=c(1,3))
+     obsDateTime <- strptime(loads[,1],format="%m/%d/%Y %H:%M:%S",tz=timeZone)
+
+     # Check for Daylight Savings Time transitions; handle with care     
+     if(length(unique(obsDateTime$isdst))!=1){  # "If today is a transition..." 
+          if(nrow(loads)==nZones*(nHrs+1)){     # "If today has 25 hours..."
+               obsDateTime$isdst[2*nZones+1:nZones] <- 0  # Reset 3rd hr to EST
+          }
+     }
+     
+     # Create an xts object containing all load obs retrieved thus far
+     temp.xts <- as.xts(cbind(loads[,3],loads[,5]), order.by=as.POSIXct(obsDateTime))
+     if(fileNumber==0){
+          allObs.xts <- temp.xts
+     } else {
+          allObs.xts <- rbind(allObs.xts,temp.xts)
+     }
+     
+}
+
+# dfColnames <- c('ID','issuedDateTime','validDateTime','zone','loadMW')
+colnames(allObs.xts) <- c('zone','loadMW')
+
+#     allObs.xts<- rbind.xts(allObsInit.xts,temp.xts,tzone=timeZone)
+     
+#     str(temp.xts)     
+
+head(allObs.xts,2)     
+tail(allObs.xts,2)
+nrow(allObs.xts['2012-11-04 01'])
+nrow(allObs.xts)
+allObs.xts['2012-11-04 01']
+     
 # data.frame(row.names='ID',check.rows=TRUE,check.names=TRUE)
 # long.df <- data.frame()
 
@@ -86,59 +130,39 @@ for(fileNumber in 1:nFiles){
 }
 
 
-loads <- read.table(URL,header=TRUE,sep=",",as.is=c(1))
-obsDateTime <- strptime(loads[,1],format="%m/%d/%Y %H:%M:%S",tz=timeZone)
-# Check for Daylight Savings Time transitions; handle with care     
-if(length(unique(obsDateTime$isdst))!=1){  # "If today is a transition..." 
-     if(nrow(loads)==nZones*(nHrs+1)){     # "If today has 25 hours..."
-          obsDateTime$isdst[2*nZones+1:nZones] <- 0  # Reset 3rd hr to EST
-     }
-     #     if(nrow(loads)==nZones*(nHrs-1)){
-     #         # Spring forward          
-     #     }
-}
-
-
-colNames <- c('obsDateTime','zone','zone#','load')
-temp.df <- cbind(obsDateTime,loads[,3:5])
-colnames(temp.df) <- colNames
-str(temp.df[,1])
-str(obsDateTime)
-temp.df[1:35,1]
-head(temp.df,35)
-str(temp.df)
-
-N <- nrow(big.array) # = length(allValidDates)
-rowsWithNAs <- c(1:5,(N-4):N)
-rowsWithNAs 
-big.array[-rowsWithNAs,,'N.Y.C.','hr16']
-dim(big.array)
-dim(big.array[-rowsWithNAs,,,])
-big.array <- big.array[-rowsWithNAs,,,]
-dimnames(big.array)
-
-big.array[,1,1,1]
-attributes(big.array)
-
-big.array['Valid 2012-11-04',,'N.Y.C.','hr16']
-big.array[,,'N.Y.C.','hr16']
-small.array <- big.array[,,'N.Y.C.','hr16']
-rowTest
-rowTest <- c('Valid 2012-11-04','Valid 2012-11-05')
-small.array[rowTest, ]
-
-small.array[, 'lag5']['Valid 2012-11-04']
-dimnames(small.array)
-
-save(big.array,file='load_forecasts.dat')
-
-# length(big.array[is.na(big.array['Valid 2011-03-13',,'N.Y.C.',1])==TRUE])
-# test <- c('Valid 2011-03-12','Valid 2011-03-13')
-# big.array[is.na(big.array[test,-7,'N.Y.C.',1])==TRUE]
-# attributes(big.array)$dim
-
-# 
 # ######### DEPRECATED CODE ############
+
+#      N <- nrow(big.array) # = length(allValidDates)
+#      rowsWithNAs <- c(1:5,(N-4):N)
+#      rowsWithNAs 
+#      big.array[-rowsWithNAs,,'N.Y.C.','hr16']
+#      dim(big.array)
+#      dim(big.array[-rowsWithNAs,,,])
+#      big.array <- big.array[-rowsWithNAs,,,]
+#      dimnames(big.array)
+#      
+#      big.array[,1,1,1]
+#      attributes(big.array)
+#      
+#      big.array['Valid 2012-11-04',,'N.Y.C.','hr16']
+#      big.array[,,'N.Y.C.','hr16']
+#      small.array <- big.array[,,'N.Y.C.','hr16']
+#      rowTest
+#      rowTest <- c('Valid 2012-11-04','Valid 2012-11-05')
+#      small.array[rowTest, ]
+#      
+#      small.array[, 'lag5']['Valid 2012-11-04']
+#      dimnames(small.array)
+#      
+#      save(big.array,file='load_forecasts.dat')
+#      
+#      # length(big.array[is.na(big.array['Valid 2011-03-13',,'N.Y.C.',1])==TRUE])
+#      # test <- c('Valid 2011-03-12','Valid 2011-03-13')
+#      # big.array[is.na(big.array[test,-7,'N.Y.C.',1])==TRUE]
+#      # attributes(big.array)$dim
+#      
+#      # 
+     
 # test.array[ ,'obs'][is.na(test.array[,'obs']==TRUE)]
 # is.na(test.array[,'obs']==TRUE
 #      
