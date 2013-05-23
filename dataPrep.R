5ee8a6f77ca08cc9a836c5a86d445edfa7bac58e
arthursmalliii <arthur.small@ventirisk.com>
2013-05-22 20:24:48 -0400

    Using old file dataPrep.R as test to work out github sync bugs.

--- dataPrep.R
+++ dataPrep.R
@@ -1,123 +1,109 @@
 #  File dataPrep.R
-#  Fetches NYISO load forecasts and obs from local directory, creates data array
-#  Array dimensions are: 
-#    dateTime validDate: date for which forecast is valid. Class: POSIX. Range: period of record
-#    zone zone = 1, ..., 11  : NYISO load control region. Class: factor. 
-#    forecast lag time k=1,..., 6; number of days between forecast issued-on and valid-for dates. Class: factor.
-#  Entries are forecasts (for k > 0) and obs (k=0) of load in MWh/hr
-#library(plyr)
+#  Fetches NYISO load forecasts and obs from local CSV files, organizes these into two xts objects
+#  Each value represents load (in MWh/hr) for one hour, for one NYISO zone. 
+
 library(xts)
-load('zoneData.rda')
+library(reshape2)
+#library(plyr)
 source('NYISO_ETL_utilities.R')
 
 # Initialize parameters ---------------------------------------------------
 
+load('zoneData.rda')
+
 timeZone <- 'America/New_York'
 Sys.setenv(TZ=timeZone)
-nZones <- nrow(zoneData)  # Number of NYISO load control zones
+seriesStartDate <- ISOdate(2012,10,1,0,0,0,tz=timeZone)
+seriesEndDate   <- ISOdate(2013,2,25,0,0,0,tz=timeZone)
 SecsPerHr <- 3600 
 
-nLags <-  6  # Number of days of forecasts in each NYISIO file
+nZones <- nrow(zoneData)  # Number of NYISO load control zones
+nLags <-  6               # Number of days of forecasts in each NYISIO forecast file
 
 # Locations of NYISO forecast and load files
-fcstURLdir <- "http://mis.nyiso.com/public/csv/isolf/"
-obsURLdir  <- "./Data/"
-# obsURLdir <- "http://mis.nyiso.com/public/csv/palIntegrated/"
+fcstDirURL <- "http://mis.nyiso.com/public/csv/isolf/"
+obsDirURL  <- "./Data/"
+# obsDirURL <- "http://mis.nyiso.com/public/csv/palIntegrated/"
 
-seriesStartDate <- ISOdate(2012,10,17,0,0,0,tz=timeZone)
-seriesEndDate   <- ISOdate(2012,10,17,0,0,0,tz=timeZone)
+
+# Begin main program ------------------------------------------------------
+
+#  Prepare object 'recentFcsts' in which to store forecasts in between their respective "issued on" dates and "valid for" dates. 'recentFcsts' is structured as a list of data frames, one for each lag in 1:nLags
+
+maxHrsAhead   <- nLags*24+1   # Extra hour needed for annual DST transitions
+
+recentFcsts <- emptyFcstList(nLags=nLags,nrow=maxHrsAhead,ncol=nZones,colnames=zoneData$names,periodName='hour',lagsAreCalled='days')
+
 nFiles <- as.integer(seriesEndDate - seriesStartDate)+1
-
-# Make object 'fcsts.xts' containting all forecasts and obs ----------------------------
-#   Data are to be organized in an xts object in which each row corresponds
-#   to one hour's obs and six lagged forecasts of that obs, for one zone.
-
-fcst.array <- array(dim=c(nLags,nLags*24+1,1+nZones))
-dimnames(fcst.array)[[3]] <- c("Valid for",zoneData$names)
-
 today  <- seriesStartDate
 for(fileCount in 0:(nFiles-1)){
-     Yr  <- strftime(today,format='%Y')
-     Mo  <- strftime(today,format='%m')
-     Day <- strftime(today,format='%d')
-     
-     # Fetch today's obs from a local directory
-     obsURL <- paste(obsURLdir,Yr,Mo,"01palIntegrated_csv/",Yr,Mo,Day,"palIntegrated.csv", sep="")
-     obs.df <- read.table(obsURL,header=TRUE,sep=",",as.is=c(1))
- 
-     todaysObs.xts <- obsdf2xts(obs.df)
-     obsDateTime <- index(todaysObs.xts)
-     nHrs <- length(unique(obsDateTime))
-     
-     # Fetch the forecasts issued yesterday, located in a file named for today's date
-     fcstURL <- paste(fcstURLdir,Yr,Mo,Day,"isolf.csv", sep="")
-     fcstsIssuedYesterday.df <- read.table(fcstURL, header = TRUE, sep = ",")
 
-     # Load the forecasts issued yesterday into the lag=1 slot of the forecast array
-     x <- fcstsIssuedYesterday.df[,1:(nZones+1)]
-     fcst.array[1,1:nrow(fcstsIssuedYesterday.df),1:(nZones+1)]  <- as.matrix(x)
-     
-     # Extract all the forecasts valid for today
-     forecastsValidForToday.3D <- fcst.array[,1:nHrs,2:(nZones+1)]
-     
-     # Add lagged forecasts as columns in a merged xts object
-     merged.xts <- todaysObs.xts
-     for(lag in 1){
-          x <- as.integer(as.vector(forecastsValidForToday.3D[lag,,]))
-          hr <- 1     
-          y <- x[hr + 25*(0:(nZones-1))]
-          for(hr in 2:nHrs){
-               y <- c(y,x[hr + 25*(0:(nZones-1))])
-          }
-          y.xts <- as.xts(y, order.by=obsDateTime)
+     # Fetch today's obs, re-format as an xts object
+     todaysObs.xts <- getOneDaysObs(date=today,dirURL=obsDirURL)
+     nHrs <- length(unique(index(todaysObs.xts)))
 
-          # Merge today's obs and forecasts into a combined xts object
-          merged.xts <- merge(todaysObs.xts,y.xts,tzone=timeZone)
+     # Create an xts object containing only the dateTime index and the zone IDs;
+     # to be used as scaffold used to create xts objects of forecasts valid for today
+     scaffold.xts <- todaysObs.xts[,-2]
+     
+     # Fetch all the forecasts issued the previous day...
+     fcstsIssuedYesterday.df<- getOneDaysFcsts(today,dirURL=fcstDirURL)
+
+     # ... and load them into the first (lag=1) slot of the 'recentFcsts' list
+     recentFcsts[[1]][1:nrow(fcstsIssuedYesterday.df),]  <- fcstsIssuedYesterday.df[,1+1:nZones]
+     
+     # Subset 'recentFcsts' to extract only the forecasts valid sometime today
+     fcstsValidToday.lst <- lapply(seq(recentFcsts), function(lag){
+          topRows <- data.frame(recentFcsts[[lag]][1:nHrs,])
+          return(topRows)
+     })
+
+     # Re-organize forecasts valid for today into an xts object
+     fcstsValidToday.xts <- fcstList2xts(fcstsValidToday.lst,scaffold.xts)
+          
+     # Create or expand xts objects containing all forecasts and obs retrieved so far
+     if(today==seriesStartDate){ #  If this is the first day in the series...
+          # Create xts objects to contain obs and fcsts 
+          obs.xts <- todaysObs.xts
+          forecasts.xts <- fcstsValidToday.xts  
+     } else {                    # If not... 
+          # Append today's data in rows below those from earlier dates
+          obs.xts <- rbind(obs.xts,todaysObs.xts)
+          forecasts.xts <- rbind(forecasts.xts,fcstsValidToday.xts)
      }
      
-     
-     # Add today's forecasts and obs to those retrieved earlier for previous dates
-     if(fileCount==0){ #  If this is the first day in the series...
-          # Create an xts object to contain all obs and fcsts     
-          fcsts.xts <- merged.xts  
-     } else {           # If not... 
-          # Append today's data to those from earlier dates
-          fcsts.xts <- rbind(fcsts.xts,merged.xts)
+     # Prepare for next day's data: 
+     # If(nLags > 1) then: In recentFcsts list:
+     #  - shift forecasts over by 1 lag
+     #  - shift all rows up nHrs rows
+     #  - set all other cells to NA
+     if(nLags > 1){
+          recentFcsts[2:nLags] <- recentFcsts[1:(nLags-1)]     
+          newRows <- 1:(maxHrsAhead-nHrs)
+          oldRows <- (1+nHrs):maxHrsAhead
+          lapply(2:nLags, function(lag){
+               recentFcsts[[lag]][newRows,] <- recentFcsts[[lag]][oldRows,]
+               recentFcsts[[lag]][-newRows,1:nZones] <- NA
+          } )          
      }
-     
-     # Prepare for next day's data:
-     # Shift forecasts over by 1 lag
-     # Clear the rest of the cells in fcst.array
-     # [...]
-     #  Temporary:
-     fcst.array <- array(dim=c(nLags,nLags*24+1,1+nZones))
-     dimnames(fcst.array)[[3]] <- c("Valid for",zoneData$names)
+     recentFcsts[[1]][1:maxHrsAhead,1:nZones] <- NA
      
      # Advance the clock by one day
      today <- today + nHrs*SecsPerHr
 }
 
-nLags <- 1          # Temporary: When finished coding, will reset to 6
-
-# Order columns by decreasing lags, then obs
-fcsts.xts <- fcsts.xts[,c(1,(nLags+2):2)]  
-colnames(fcsts.xts) <- c('zone',paste('lag',nLags:1,sep=""),'obs')
- 
 # Create an xts object of load forecasting errors
-errors.xts <- forecastErrors(f.xts,o.xts)
+errors.xts <- forecastErrors(forecasts.xts,obs.xts,obsCol=2,fcstCols=(1+1:nLags),IDCols=1)
 
-head(errors.xts)
 
+# To be added later: Create an xts object of forecast refinements
+# updates.xts <- diffs(errors.xts)  # Still need to write this function
+
+# Save the new xts objects in an R data file
+list=c('forecasts.xts','obs.xts','errors.xts')
 # list=c('fcsts.xts','obs.xts','errors.xts','updates.xts')
-list=c('fcsts.xts','obs.xts','errors.xts')
+
+
 save(list=list,file='NYISO_load_forecast_evaluation_dataset.rda')
 
-
-# ######### DEPRECATED CODE ############
-
-# # m = c(10,20,30) # means for each of the 3 point locations
-# Reps <- length(space)*length(dateTime)/length(m)
-# mydata = rnorm(length(space)*length(dateTime),mean=rep(m, Reps))
-# IDs = paste("ID",1:length(mydata))
-# #  mydata = data.frame(values = signif(mydata,3), ID=IDs)
-# # length(mydata$values)+length(mydata$ID)
+# ######### END OF CODE ############
